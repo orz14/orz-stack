73dafb0402863342f69cad7893726befe66985ce
Oriz Wahyu N <oriezt.id@gmail.com>
2023-12-24 14:43:07 +0700

    ui: add profile dropdown menu

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -8,12 +8,4 @@
     <x-card>
         {{ __("You're logged in!") }}
     </x-card>
-
-    <form action="{{ route('logout') }}" method="POST" class="mt-4">
-        @csrf
-
-        <x-button class="bg-gray-800 hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900" onclick="return confirm('Are you sure want to logout?')">
-            {{ __('Logout') }}
-        </x-button>
-    </form>
 @endsection
